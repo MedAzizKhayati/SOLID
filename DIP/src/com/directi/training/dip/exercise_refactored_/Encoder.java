98fc9f88ab05def08a09a8092527047438cab325
MedAzizKhayati <mohamedazizkhayati@insat.u-carthage.tn>
2023-02-13 14:15:55 +0100

    fixes

--- DIP/src/com/directi/training/dip/exercise_refactored_/Encoder.java
+++
@@ -1,5 +0,0 @@
-package com.directi.training.dip.exercise_refactored_;
-
-interface Encoder {
-    String encode(String input);
-}
--- DIP/src/com/directi/training/dip/exercise_refactored_/Writer.java
+++
@@ -1,4 +0,0 @@
-package com.directi.training.dip.exercise_refactored_;
-interface Writer {
-    void write(String input);
-}
